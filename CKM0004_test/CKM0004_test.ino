05a572570e960e56a9f72c277873cb4f25b8709d
Cokoino <37071399+Cokoino@users.noreply.github.com>
2019-05-03 23:43:42 +0800

    Add files via upload

---
+++ CKM0004_test/CKM0004_test.ino
@@ -0,0 +1,15 @@
+#define L1 12
+#define L2 13
+void setup() {
+  pinMode(L1,OUTPUT);  // initialize digital pin L1 as an output.
+  pinMode(L2,OUTPUT);  // initialize digital pin L2 as an output.
+}
+
+void loop() {
+  digitalWrite(L1,HIGH);  // turn the LED on
+  digitalWrite(L2,HIGH);  // turn the LED on
+  delay(5000);            // wait for a second
+  digitalWrite(L1,LOW);   // turn the LED off
+  digitalWrite(L2,LOW);   // turn the LED off
+  delay(5000);            // wait for a second
+}
